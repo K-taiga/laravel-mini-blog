fc4b8737c88bcba2536a3045406bdef07819ae1f
K-taiga <taigakatou0329@gmail.com>
2021-06-06 15:00:06 +0900

    db周り実装

---
+++ src/database/factories/BlogFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Blog;
+use Faker\Generator as Faker;
+
+$factory->define(Blog::class, function (Faker $faker) {
+    return [
+        'title' => $this->faker->realText(20),
+        'body' => $this->faker->realText(20),
+        'updated_at' => $this->faker->dateTimeBetween('-10days','0days'),
+    ];
+});
